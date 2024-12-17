04501a38b2092e176ce824f18842dc6205046950
MrJuise <MrJuise@mail.ru>
2024-12-17 16:59:35 +0300

    Homework 4

--- module_19/task1/admin.py
+++ module_19/task1/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+
+from .models import Game, Buyer
+
+
+@admin.register(Game)
+class GameAdmin(admin.ModelAdmin):
+    list_display = ('title', 'cost', 'size')
+    list_filter = ('size', 'cost')
+    search_fields = ('title',)
+    list_per_page = 20
+
+@admin.register(Buyer)
+class BuyerAdmin(admin.ModelAdmin):
+    list_filter = ('balance', 'age')
+    list_display = ('username', 'balance', 'age')
+    search_fields = ('username',)
+    list_per_page = 30
+    readonly_fields = ('balance',)
